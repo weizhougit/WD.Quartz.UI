83508b8332c683f32caa0e0ab95cd8ae3b86f3c0
周维 <2503998380@qq.com>
2023-08-31 18:09:13 +0800

    添加 说明文档

--- WD.TestUI/Program.cs
+++ WD.TestUI/Program.cs
@@ -3,19 +3,24 @@ using WD.Quartz.UI.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 var config = builder.Configuration;
-//builder.Services.AddQuartzUI(config.GetSection("QuartzUI:Db").Get<DbOption>());
+
 builder.Services.AddQuartzUI();
+
 builder.Services.AddQuartzClass();
 
 var app = builder.Build();
+
 app.UseQuartzUILogin();
+
 app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGet("/", async context =>
     {
-        await context.Response.WriteAsync("Hello World!1212121");
+        await context.Response.WriteAsync("Hello World!");
     });
 });
+
 app.UseQuartz(); //添加这行代码
+
 app.Run();
